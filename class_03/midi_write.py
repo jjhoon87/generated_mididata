603433d63e10dd3b837411b0781a05c33fc5b796
jjhoon87 <69728587+jjhoon87@users.noreply.github.com>
2022-07-05 13:17:42 +0900

    Update midi_write.py

--- class_03/midi_write.py
+++ class_03/midi_write.py
@@ -1,13 +1,10 @@
+import midiutil
 def write_midi(seq, bpm, path):
     mf = midiutil.MIDIFile(1, file_format=1)
     track = 0
     channel = 0
     mf.addTempo(track, 0, bpm)
     for i, n in enumerate(seq):
-        print(i, n)
-        d = n['duration_time']
-        # if len(str(d)) > 10:
-        #     d -= .0001
         mf.addNote(
             track,
             channel,
